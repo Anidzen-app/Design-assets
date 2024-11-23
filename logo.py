1b39084a102374fd8a0c7a4c6a4057e52d39183c
Vahe Sargsyan <140258231+w33bvGL@users.noreply.github.com>
2024-11-23 11:31:13 +0400

    add references

--- logo.py
+++ logo.py
@@ -1,6 +1,5 @@
 from PIL import Image, ImageDraw, ImageFont
 
-# Create a blank canvas
 canvas_size = (1000, 1000)
 background_color = (255, 255, 255)
 image = Image.new("RGB", canvas_size, background_color)
