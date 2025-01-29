de70d752251abd2c395bb4a38dd29e31e5600851
w33bvGL <w33bv.gl@gmail.com>
2025-01-29 22:11:48 +0400

    remove python scripts

--- logo.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image, ImageDraw, ImageFont
-
-canvas_size = (1000, 1000)
-background_color = (255, 255, 255)
-image = Image.new("RGB", canvas_size, background_color)
-draw = ImageDraw.Draw(image)
-
-image.show()
